013c398782c7f4c5098f02efb01d3b293786d333
Siobhan Branagh <siobhan.branagh@gmail.com>
2026-06-14 14:06:27 +0100

    added to ability to post form

--- src/contact.jsx
+++ src/contact.jsx
@@ -6,7 +6,13 @@ function ContactPage() {
                 <h2>Contact</h2>
                 <p>Feel free to get in touch with me using the form below</p>
                 <div >
-                    <form>
+                    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+                        <input type="hidden" name="form-name" value="contact" />
+                         <p class="hidden">
+                            <label>
+                            Don’t fill this out if you’re human: <input name="bot-field" type="text" />
+                            </label>
+                        </p>
                         <label for="name">Name:</label>
                         <input type="text" id="name" name="name" required />
                         <label for="email">Email:</label>
